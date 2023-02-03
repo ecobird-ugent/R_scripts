2fbc561283b56ec5f315c483f6d1b9764515fed7
ReinoudAllaert <71331251+ReinoudAllaert@users.noreply.github.com>
2023-02-03 09:17:21 +0100

    add DB

--- preprocess_mortality.R
+++ preprocess_mortality.R
@@ -4,48 +4,64 @@ require(dplyr)
 require(data.table)
 
 # (meta-)data
-animals <- read_sheet("https://docs.google.com/spreadsheets/d/1NZnLRdTBtqpktkQ3bjbwUPja7BXfkYZPvGqJplTR-Fs/edit#gid=0")
+animals <-
+  read_sheet(
+    "https://docs.google.com/spreadsheets/d/1NZnLRdTBtqpktkQ3bjbwUPja7BXfkYZPvGqJplTR-Fs/edit#gid=0"
+  )
 GPS_data <- fread("~/Downloads/HG_JUVENILE.csv")
 
 
 # remove undeployed tags
-animals <- subset(animals, !is.na(project))
+animals <- subset(animals,!is.na(project))
 
 
 # movebank metadata to proper columns
-animals <- separate(data = animals, col = track_session_remarks, into = c("wing_length", "head_length", "tarsus_length", "death_info"), sep = "\\|")
-animals$wing_length <- sub(".*:", "", animals$wing_length)
-animals$tarsus_length <- sub(".*:", "", animals$tarsus_length)
-animals$head_length <- sub(".*:", "", animals$head_length)
+animals <-
+  separate(
+    data = animals,
+    col = track_session_remarks,
+    into = c("wing_length_rel", "head_length_rel", "tarsus_length_rel", "death_info"),
+    sep = "\\|"
+  )
+animals$wing_length_rel <- sub(".*:", "", animals$wing_length_rel)
+animals$tarsus_length_rel <- sub(".*:", "", animals$tarsus_length_rel)
+animals$head_length_rel <- sub(".*:", "", animals$head_length_rel)
 
 # remove malfunctional tags
-animals <- subset(animals, !grepl("malfunc", animals$death_info, fixed = TRUE))
+animals <-
+  subset(animals,!grepl("malfunc", animals$death_info, fixed = TRUE))
 
 # cause of death to proper format
 animals <- animals %>%
-  mutate(death_info_cleaned = case_when(grepl("no further", animals$death_info) ~ 'No information',
-                                grepl("fox", animals$death_info) ~ 'Fox',
-                                grepl("traffic", animals$death_info) ~ 'Roadkill',
-                                grepl("turbine", animals$death_info) ~ 'Wind turbine',
-                                grepl("field", animals$death_info) ~ 'No information'))
+  mutate(
+    death_info_cleaned = case_when(
+      grepl("no further", animals$death_info) ~ 'No information',
+      grepl("fox", animals$death_info) ~ 'Fox',
+      grepl("traffic", animals$death_info) ~ 'Roadkill',
+      grepl("turbine", animals$death_info) ~ 'Wind turbine',
+      grepl("field", animals$death_info) ~ 'No information'
+    )
+  )
 
 # remove living birds
-animals <- subset(animals, !is.na(death_info_cleaned))
+animals <- subset(animals,!is.na(death_info_cleaned))
 
 
 # subset GPS-data to only dead birds
-GPS_data <- subset(GPS_data, `individual-local-identifier` %in% animals$metal_ring)
+GPS_data <-
+  subset(GPS_data,
+         `individual-local-identifier` %in% animals$metal_ring)
 
 # fix timestamp
 GPS_data$timestamp <- as.POSIXct(GPS_data$timestamp)
 
 # retain week of data leading up to death by looping over every individual
 GPS_death <- NULL
-for (bird in unique(GPS_data$`individual-local-identifier`)){
+for (bird in unique(GPS_data$`individual-local-identifier`)) {
   temp <- subset(GPS_data, `individual-local-identifier` == bird)
   stop_date <- subset(animals, metal_ring == bird)
   stop_date <- as.POSIXct(stop_date$track_session_end_date)
-  start_date <- stop_date - 7*24*60*60
+  start_date <- stop_date - 7 * 24 * 60 * 60
   temp <- subset(temp, timestamp >= start_date)
   temp <- subset(temp, timestamp <= stop_date)
   GPS_death <- rbind(GPS_death, temp)
@@ -61,7 +77,8 @@ t2 <- as.data.frame(table(t$`individual-local-identifier`))
 # left with 28 individuals (from 38) for which we have a complete week of data
 t2 <- subset(t2, Freq == 8)
 
-GPS_death <- subset(GPS_death, `individual-local-identifier` %in% t2$Var)
+GPS_death <-
+  subset(GPS_death, `individual-local-identifier` %in% t2$Var)
 t <- GPS_death %>% count(`individual-local-identifier`)
 
 # if we only want data with an average resolution of 20min: 18 birds left
@@ -69,5 +86,6 @@ t <- GPS_death %>% count(`individual-local-identifier`)
 t <- subset(t, n >= 330)
 
 # leaves us with this dataset to calculate from:
-GPS_death <- subset(GPS_death, `individual-local-identifier` %in% t$`individual-local-identifier`)
-
+GPS_death <-
+  subset(GPS_death,
+         `individual-local-identifier` %in% t$`individual-local-identifier`)
