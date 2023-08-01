3ca2269d028d177d0472e1a70f3fb02b8029eb4d
ReinoudAllaert <71331251+ReinoudAllaert@users.noreply.github.com>
2023-08-01 11:21:25 +0200

    Update dead_gull_finder.R

--- dead_gull_finder.R
+++ dead_gull_finder.R
@@ -8,60 +8,78 @@ library(pacman)
 packages <- c("move", "dplyr", "keyring", "googlesheets4", "leaflet", "htmltools", "RColorBrewer")
 p_load(character.only = TRUE, packages)
 
-# movebank data
+# movebank (meta)data
 GPS_data <- getMovebankLocationData(study=2217728245 , sensorID="GPS", 
                                     login=movebankLogin(username="Reinoud Allaert", password=key_get("movebank", "Reinoud Allaert")), 	
-                                      timestamp_start = as.POSIXct(Sys.Date()-7))
+                                      timestamp_start = as.POSIXct(Sys.Date()-4))
 
-# metadata 
-animal_meta <-
-  read_sheet(
-    "https://docs.google.com/spreadsheets/d/1NZnLRdTBtqpktkQ3bjbwUPja7BXfkYZPvGqJplTR-Fs/edit?usp=sharing"
-  )
+meta_data <- getMovebankReferenceTable(study=2217728245 ,  
+                                    login=movebankLogin(username="Reinoud Allaert", password=key_get("movebank", "Reinoud Allaert")))
 
-
-# filter metadata based on date (only vogels Jolien)
-animal_meta <- animal_meta %>%
-  subset(as.Date(track_session_start_date) >= as.Date("2023-07-01") &
-           as.Date(track_session_start_date) <= as.Date("2023-08-01"))
+# only meta of deployed tags
+meta_data <- meta_data %>% filter(!is.na(animal_timestamp_start) & number_of_location_events >= 10)
 
 # filter main gps data
 GPS_data <- GPS_data %>%
-  subset(individual.local.identifier %in% unique(animal_meta$metal_ring))
+  subset(individual.id %in% unique(meta_data$animal_id))
 
-# Find birds in animal_meta that have no GPS data in the last 7 days
-missing_birds <- anti_join(animal_meta, GPS_data, by = c("serial_number" = "tag.local.identifier"))
-
-# Extract tag_id for missing birds
-missing_tag_ids <- unique(missing_birds$serial_number)
-
-# Print the tag_id of missing birds
-print(missing_tag_ids)
-
-# check birds that show no movement
-# daily distance in km
+# calculate daily distance per bird
 daily_distance <- GPS_data %>%
   group_by(individual.local.identifier, date = as.Date(timestamp)) %>%
   summarize(daily_distance = tryCatch(sum(distHaversine(cbind(location.long, location.lat))), error = function(e) NA)/1000)
 
-# keep birds without data or under 10km/day
-daily_distance <- daily_distance %>%
-  filter(is.na(daily_distance) | daily_distance < 5)
+
+# calculate the average daily distance for each individual
+average_distance <- daily_distance %>%
+  group_by(individual.local.identifier) %>%
+  summarize(
+    avg_daily_distance = mean(daily_distance, na.rm = TRUE),
+    days_available = sum(!is.na(daily_distance))
+  )
+
+# determine if each individual has data for at least 3 out of 4 days
+alive_birds <- average_distance %>%
+  filter(days_available >= 3)
+
+# check if the average daily distance for each individual is > 1 km per day
+alive_birds <- alive_birds %>%
+  filter(avg_daily_distance > 1)
+
+# split the dataframe into two groups: alive birds and dead birds
+alive_individuals <- alive_birds$individual.local.identifier
+alive_df <- daily_distance %>%
+  filter(individual.local.identifier %in% alive_individuals)
+alive_df <- unique(alive_df$individual.local.identifier)
+alive_df
+
+
+dead_individuals <- setdiff(daily_distance$individual.local.identifier, alive_individuals)
+dead_df <- daily_distance %>%
+  filter(individual.local.identifier %in% dead_individuals)
+dead_df <- unique((dead_df$individual.local.identifier))
+dead_df
+
+# Print the results
+print("Alive Birds:")
+print(alive_df)
+
+print("Dead Birds:")
+print(dead_df)
 
 # keep last 50 positions for every bird
 GPS_data <- GPS_data %>%
   group_by(individual.local.identifier) %>%
-  slice_tail(n = 50) %>%
+  slice_tail(n = 10) %>%
   ungroup()
 
 # filter to only problematic birds
 GPS_data <- GPS_data %>%
-  subset(individual.local.identifier %in% unique(daily_distance$individual.local.identifier))
+  subset(individual.local.identifier %in% dead_df)
 unique(GPS_data$tag.local.identifier)
 
 ### plotmap
 
-lbls <- paste(GPS_data$tag.local.identifier)
+lbls <- paste(GPS_data$tag.local.identifier, " ",GPS_data$timestamp )
 qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 col=sample(col_vector, 62)
